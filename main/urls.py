f1e106e1a1a81181040bc3824d8bf8a4adc69a98
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2023-10-17 15:30:49 +0300

    auth urls

--- main/urls.py
+++ main/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("user/", include("user_profile.urls")),
+    path("api/", include("user_profile.urls")),
+    path("api/", include("auth.urls")),
 ]
