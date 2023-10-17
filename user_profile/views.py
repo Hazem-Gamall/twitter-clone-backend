9d21ef575a38dbd8293a6c3a7849fcd126689e6a
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2023-10-17 15:30:26 +0300

    registeration view

--- user_profile/views.py
+++ user_profile/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render, HttpResponse
 from rest_framework import viewsets
 from .serializers import UserSerializer
 from .models import UserProfile
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
 
 
 # Create your views here.
-class UserViewSet(viewsets.ModelViewSet):
+class UserRegisterationViewSet(
+    viewsets.mixins.CreateModelMixin, viewsets.GenericViewSet
+):
+    permission_classes = [AllowAny]
     serializer_class = UserSerializer
-    queryset = UserProfile.objects.all()
