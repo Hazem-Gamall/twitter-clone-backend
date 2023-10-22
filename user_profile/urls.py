9ee971cdf17fc242d92911b88f6a0f44e43ea4b5
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2023-10-22 20:54:13 +0300

    enpoint for accessing the posts for a specific user

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+from rest_framework_nested.routers import NestedSimpleRouter
 
-router = routers.DefaultRouter()
+router = routers.SimpleRouter()
 router.register(r"users", views.UserViewSet)
+
+posts_router = NestedSimpleRouter(router, r"users", "post")
+
 urlpatterns = router.urls
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, HttpResponse
+from rest_framework.response import Response
 from rest_framework import viewsets, mixins
 from user_profile.models import UserProfile
 from rest_framework.decorators import action
 from user_profile.permissions import IsAuthenticatedOrCreateOrOptions
 from .serializers import UserProfileSerializer
+from posts.serializers import PostSerializer
 
 
 # Create your views here.
@@ -12,3 +13,9 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
     lookup_field = "user__username"
+
+    @action(["GET"], detail=False, url_path="(?P<user__username>[^/.]+)/posts")
+    def posts(self, request, user__username):
+        posts = self.queryset.get(user__username=user__username).posts.all()
+        serialized_posts = PostSerializer(posts, many=True).data
+        return Response(serialized_posts)
