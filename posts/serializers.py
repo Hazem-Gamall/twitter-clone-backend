1c5e3a230c1136159b1dc81c28d1c37c1f90c7ec
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2023-10-22 21:53:57 +0300

    posts can now be created with username

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import serializers
 from main.serializer_validation_mixins import ReadOnlyOrUnkownFieldErrorMixin
 from .models import Media, Post
 from user_profile.serializers import UserProfileSerializer
+from user_profile.models import UserProfile
 
 
 class MediaSerialzer(serializers.ModelSerializer):
@@ -29,21 +32,47 @@ class UpdatePostSerializer(
 
 class PostSerializer(ReadOnlyOrUnkownFieldErrorMixin, serializers.ModelSerializer):
     media = MediaSerialzer(many=True, allow_null=True, required=False)
-    user = serializers.SerializerMethodField("get_user")
+    post_user = serializers.SerializerMethodField("get_post_user")
+    username = serializers.CharField(
+        max_length=35, source="user__user__username", required=False
+    )
 
     class Meta:
         model = Post
         fields = "__all__"
-        read_only_fields = ["replies"]
+        read_only_fields = ["replies", "post_user"]
+        extra_kwargs = {
+            "user": {"write_only": True, "required": False},
+            "username": {"write_only": True, "requrequiredired": False},
+        }
 
-    def get_user(self, obj):
+    def get_post_user(self, obj):
         return {
+            "id": obj.id,
+            "name": obj.user.user.first_name,
             "username": obj.user.user.username,
             "avatar": obj.user.avatar if obj.user.avatar else None,
         }
 
     def create(self, validated_data):
-        post = Post.objects.create(**validated_data)
+        if "user__user__username" in validated_data:
+            username = validated_data.pop("user__user__username")
+            try:
+                user = UserProfile.objects.get(user__username=username)
+            except:
+                raise ValidationError(
+                    {
+                        "username": [
+                            f"Invalid username '{username}' - object does not exist."
+                        ]
+                    }
+                )
+            print("user", user)
+            post = Post.objects.create(user=user, **validated_data)
+        elif "user" in validated_data:
+            post = Post.objects.create(**validated_data)
+        else:
+            raise ValidationError("You must provied a user or username field")
 
         if "media" in validated_data:
             media_data = validated_data.pop("media")
