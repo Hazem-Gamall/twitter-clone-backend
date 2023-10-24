f99529009f39fcc4dd407f9cbb40a1972e22bc15
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2023-10-24 16:39:08 +0300

    configured media directory

--- main/urls.py
+++ main/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -23,3 +25,6 @@ urlpatterns = [
     path("api/", include("user_auth.urls")),
     path("api/", include("posts.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
