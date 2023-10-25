1264e8985e0b858d706206bbde47d67e80aec1d4
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2023-10-25 14:05:14 +0300

    configured nested viewset for reposts

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -10,10 +10,14 @@ posts_router = NestedSimpleRouter(router, r"users", lookup="posts")
 posts_router.register(r"posts", views.UserPostsViewSet, basename="user-posts")
 
 likes_router = NestedSimpleRouter(router, r"users", lookup="likes")
-likes_router.register(r"likes", views.UserLikedPostsViewSet, basename="user-likes")
+likes_router.register(r"likes", views.UserLikesPostsViewSet, basename="user-likes")
+
+repost_router = NestedSimpleRouter(router, r"users", lookup="repost")
+repost_router.register(r"repost", views.UserRepostPostsViewSet, basename="user-repost")
 
 urlpatterns = [
     path(r"", include(router.urls)),
     path(r"", include(posts_router.urls)),
     path(r"", include(likes_router.urls)),
+    path(r"", include(repost_router.urls)),
 ]
