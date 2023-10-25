eae0b0b8ecfd6f7e0f7f9cd8559b0cba0002cad4
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2023-10-25 23:31:05 +0300

    endpoint to get post replies

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,15 @@
+from django.urls import path, include
 from rest_framework import routers
 from . import views
+from rest_framework_nested.routers import NestedSimpleRouter
 
-router = routers.DefaultRouter()
+router = routers.SimpleRouter()
 router.register(r"posts", views.PostViewSet)
 
-urlpatterns = router.urls
+replies_router = NestedSimpleRouter(router, r"posts", lookup="post")
+replies_router.register(r"replies", views.PostRepliesViewSet, basename="post-replies")
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("", include(replies_router.urls)),
+]
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from .serializers import PostSerializer, UpdatePostSerializer
 from .models import Post
-from rest_framework import permissions
+from rest_framework import permissions, status, response, exceptions
 
 # Create your views here.
 
@@ -15,3 +16,18 @@ class PostViewSet(viewsets.ModelViewSet):
         if self.action in ["update", "partial_update"]:
             return UpdatePostSerializer
         return PostSerializer
+
+
+class PostRepliesViewSet(viewsets.ViewSet):
+    queryset = Post.objects.all()
+
+    def list(self, request, post_pk):
+        try:
+            post = self.queryset.get(id=post_pk)
+        except:
+            raise exceptions.ValidationError(
+                {"post": "The id provided does not match any known post"}
+            )
+        print(post.replies.all())
+        serialized_replies = PostSerializer(post.replies.all(), many=True).data
+        return response.Response(serialized_replies)
